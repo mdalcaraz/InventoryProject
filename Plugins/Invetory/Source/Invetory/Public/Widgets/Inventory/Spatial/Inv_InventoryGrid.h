94167f5ed022f8a758c53c0b90ac36e9ec4ad50f
mdalcaraz <mdalcaraz@live.com>
2025-07-22 10:24:06 -0300

    inventory HUD

--- Plugins/Invetory/Source/Invetory/Public/Widgets/Inventory/Spatial/Inv_InventoryGrid.h
+++ Plugins/Invetory/Source/Invetory/Public/Widgets/Inventory/Spatial/Inv_InventoryGrid.h
@@ -6,7 +6,8 @@
 #include "Blueprint/UserWidget.h"
 #include "Types/Inv_GridTypes.h"
 #include "Inv_InventoryGrid.generated.h"
-
+class UInv_GridSlot;
+class UCanvasPanel;
 /**
  * 
  */
@@ -15,9 +16,30 @@ class INVETORY_API UInv_InventoryGrid : public UUserWidget
 {
 	GENERATED_BODY()
 public:
+	virtual void NativeOnInitialized() override;
 	EInv_ItemCategory GetItemCategory() const { return ItemCategory; }
 	
 private:
+	void ConstructGrid();
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"),Category="Inventory")
 	EInv_ItemCategory ItemCategory;
+
+	UPROPERTY()
+	TArray<TObjectPtr<UInv_GridSlot>> GridSlots;
+
+	UPROPERTY(EditAnywhere, Category="Inventory")
+	TSubclassOf<UInv_GridSlot> GridSlotClass;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UCanvasPanel> CanvasPanel;
+
+	UPROPERTY(EditAnywhere, Category="Inventory")
+	int32 Rows;
+
+	UPROPERTY(EditAnywhere, Category="Inventory")
+	int32 Columns;
+
+	UPROPERTY(EditAnywhere, Category="Inventory")
+	float TileSize;
 };
